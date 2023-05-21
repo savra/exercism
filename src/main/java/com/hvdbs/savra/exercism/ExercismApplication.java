9bd2ec5890e6fee346e0b912cbb091bd04a93e7e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-05-21 08:10:15 +0300

    initial commit

---
+++ src/main/java/com/hvdbs/savra/exercism/ExercismApplication.java
@@ -0,0 +1,13 @@
+package com.hvdbs.savra.exercism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hvdbs/savra/exercism/ExercismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdbs.savra.exercism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
