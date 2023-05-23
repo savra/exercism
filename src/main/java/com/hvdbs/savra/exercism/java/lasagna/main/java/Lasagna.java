ec4cf5aa4a9734f7db178a5a8d68fca5e64353a8
Sergey Savrukhin <savra.sv@yandex.ru>
2023-05-23 19:56:49 +0300

    Cook your lasagna

---
+++ src/main/java/com/hvdbs/savra/exercism/java/lasagna/main/java/Lasagna.java
@@ -0,0 +1,20 @@
+package com.hvdbs.savra.exercism.java.lasagna.main.java;
+
+public class Lasagna {
+    private static final int PREPARATION_TIME = 2;
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int remaining) {
+        return expectedMinutesInOven() - remaining;
+    }
+
+    public int preparationTimeInMinutes(int layers) {
+        return PREPARATION_TIME * layers;
+    }
+
+    public int totalTimeInMinutes(int layers, int hasBeenInOven) {
+        return preparationTimeInMinutes(layers) + hasBeenInOven;
+    }
+}
