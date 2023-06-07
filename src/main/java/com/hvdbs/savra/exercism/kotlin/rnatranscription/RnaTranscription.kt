42fc7b24f5c55ceb5769e9dbff0626c4051ff182
Sergey Savrukhin <savra.sv@yandex.ru>
2023-06-07 19:29:46 +0300

    RNA Transcription

---
+++ src/main/java/com/hvdbs/savra/exercism/kotlin/rnatranscription/RnaTranscription.kt
@@ -0,0 +1,12 @@
+fun transcribeToRna(dna: String): String = dna.splitToSequence("").map { s: String ->
+    when (s) {
+        "G" -> "C"
+        "C" -> "G"
+        "T" -> "A"
+        "A" -> "U"
+        else -> {
+            ""
+        }
+    }
+}.joinToString("")
+
